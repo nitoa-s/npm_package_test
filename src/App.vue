6194ea78c29ea3208d85068a69b956ad20f22383
nitoa-s <kitamakeai2025@gmail.com>
2023-02-12 21:17:29 +0900

    表示テスト

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <TestGloNav />
+  </div>
+</template>
+
+<script>
+import TestGloNav from './components/TestGloNav.vue'
+export default {
+  name: 'App',
+  components: { TestGloNav },
+}
+</script>
+
+<style scoped>
+/*scopedとつけるとcssのスコープがコンポーネント単位になる。*/
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
